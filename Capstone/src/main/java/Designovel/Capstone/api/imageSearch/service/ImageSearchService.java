7231ac0b16a00a0ffa03820d7644ad3cd4ae62fb
GeonHeeLeee <ghlee00125@naver.com>
2024-07-08 15:17:44 +0900

    refactor: ImageSearch 서비스 레이어 수정

--- Capstone/src/main/java/Designovel/Capstone/api/imageSearch/service/ImageSearchService.java
+++ Capstone/src/main/java/Designovel/Capstone/api/imageSearch/service/ImageSearchService.java
@@ -2,13 +2,12 @@ package Designovel.Capstone.api.imageSearch.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -22,22 +21,15 @@ public class ImageSearchService {
     private final WebClient webClient;
 
     public ResponseEntity<String> sendImageSearchRequest(MultipartFile image, String category, int offset) throws IOException {
-        byte[] imageBytes = image.getBytes();
-        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-        body.add("image_upload", new ByteArrayResource(imageBytes) {
-            @Override
-            public String getFilename() {
-                return image.getOriginalFilename();
-            }
-        });
-
-        body.add("category", category);
-        body.add("top_num", offset);
+        MultipartBodyBuilder bodyBuilder = new MultipartBodyBuilder();
+        bodyBuilder.part("image_upload", image.getResource());
+        bodyBuilder.part("category", category);
+        bodyBuilder.part("top_num", offset);
 
         Mono<String> response = webClient.post()
                 .uri("/process/image")
                 .contentType(MediaType.MULTIPART_FORM_DATA)
-                .bodyValue(body)
+                .bodyValue(BodyInserters.fromMultipartData(bodyBuilder.build()))
                 .retrieve()
                 .bodyToMono(String.class);
 
