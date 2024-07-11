4cd8fdc3fd69481a65f3ec9b9361d3eb7f5a801c
GeonHeeLeee <ghlee00125@naver.com>
2024-07-11 20:05:08 +0900

    feat: ImageSearch URL 업데이터

--- Capstone/src/main/java/Designovel/Capstone/global/config/WebClientConfig.java
+++ Capstone/src/main/java/Designovel/Capstone/global/config/WebClientConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    private static final String fastAPIUrl = "https://definite-openly-caribou.ngrok-free.app";
+    private static final String fastAPIUrl = "http://203.229.61.179:8800";
     @Bean
     public WebClient webClient(WebClient.Builder webClientBuilder) {
         return webClientBuilder
