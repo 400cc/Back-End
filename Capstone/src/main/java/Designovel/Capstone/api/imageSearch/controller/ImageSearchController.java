3186539ebb9923d38800200d1af1c73dea95a008
GeonHeeLeee <ghlee00125@naver.com>
2024-07-16 19:35:45 +0900

    refactor: ImageSearchController JSON으로 받게 변경

--- Capstone/src/main/java/Designovel/Capstone/api/imageSearch/controller/ImageSearchController.java
+++ Capstone/src/main/java/Designovel/Capstone/api/imageSearch/controller/ImageSearchController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class ImageSearchController {
     private final ImageSearchService imageSearchService;
 
     @PostMapping("/search")
-    public ResponseEntity<String> getSimilarImages(ImageSearchDTO imageSearchDTO) throws IOException {
+    public ResponseEntity<String> getSimilarImages(@RequestBody ImageSearchDTO imageSearchDTO) {
         MallTypeId.checkMallTypeId(imageSearchDTO.getMallTypeId());
         return imageSearchService.sendImageSearchRequest(imageSearchDTO);
     }
