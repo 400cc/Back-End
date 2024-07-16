6d2f8ea8a924bbb8355a3e87f4fa8639c94e2969
GeonHeeLeee <ghlee00125@naver.com>
2024-07-16 20:08:40 +0900

    refactor: ImageSearchController 파라미터 @ModelAttribute로 받게 수정

--- Capstone/src/main/java/Designovel/Capstone/api/imageSearch/controller/ImageSearchController.java
+++ Capstone/src/main/java/Designovel/Capstone/api/imageSearch/controller/ImageSearchController.java
@@ -7,10 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -24,7 +21,7 @@ public class ImageSearchController {
     private final ImageSearchService imageSearchService;
 
     @PostMapping("/search")
-    public ResponseEntity<String> getSimilarImages(@RequestBody ImageSearchDTO imageSearchDTO) {
+    public ResponseEntity<String> getSimilarImages(@ModelAttribute ImageSearchDTO imageSearchDTO) {
         MallTypeId.checkMallTypeId(imageSearchDTO.getMallTypeId());
         return imageSearchService.sendImageSearchRequest(imageSearchDTO);
     }
