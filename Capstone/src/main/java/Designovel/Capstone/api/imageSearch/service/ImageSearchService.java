c5ca551d0c2efc9ecd33dbaab112e4b87a9683aa
GeonHeeLeee <ghlee00125@naver.com>
2024-09-26 13:43:08 +0900

    feat: FastAPI 로직 변경에 맞춰 로직 수정

--- Capstone/src/main/java/Designovel/Capstone/api/imageSearch/service/ImageSearchService.java
+++ Capstone/src/main/java/Designovel/Capstone/api/imageSearch/service/ImageSearchService.java
@@ -1,13 +1,9 @@
 package Designovel.Capstone.api.imageSearch.service;
 
 import Designovel.Capstone.api.imageSearch.dto.ImageSearchDTO;
-import Designovel.Capstone.api.imageSearch.queryDSL.ImageSearchQueryDSL;
 import Designovel.Capstone.domain.category.category.CategoryService;
-import Designovel.Capstone.global.exception.CustomException;
-import Designovel.Capstone.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.MultipartBodyBuilder;
@@ -16,8 +12,6 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
-import java.util.Collections;
-import java.util.List;
 
 @Service
 @Slf4j
@@ -25,30 +19,20 @@ import java.util.List;
 public class ImageSearchService {
 
     private final WebClient webClient;
-    private final ImageSearchQueryDSL imageSearchQueryDSL;
     private final CategoryService categoryService;
     private static final String DEFAULT_CATEGORY_NAME = "apparel";
 
     public ResponseEntity<String> processImageSearch(ImageSearchDTO imageSearchDTO) {
-        List<String> styleByCategory = Collections.emptyList();
         String categoryName = DEFAULT_CATEGORY_NAME;
 
-        if (!imageSearchDTO.getCategoryList().isEmpty() && imageSearchDTO.getMallTypeId() != null) {
+        if (!imageSearchDTO.getCategoryList().isEmpty()) {
             categoryName = categoryService.findNameByCategoryIdList(imageSearchDTO.getCategoryList());
-            styleByCategory = imageSearchQueryDSL.findStyleByCategory(imageSearchDTO);
-            log.info("styleId: {}", styleByCategory);
-            validateStyleByCategory(styleByCategory);
         }
 
-        MultipartBodyBuilder request = buildImageSearchRequestBody(imageSearchDTO, categoryName, styleByCategory);
+        MultipartBodyBuilder request = buildImageSearchRequestBody(imageSearchDTO, categoryName);
         return sendImageSearchRequest(request);
     }
 
-    private void validateStyleByCategory(List<String> styleByCategory) {
-        if (styleByCategory.isEmpty()) {
-            throw new CustomException(HttpStatus.BAD_REQUEST, ErrorCode.STYLE_IMAGE_DOESNT_EXIST_FOR_CATEGORY);
-        }
-    }
 
     private ResponseEntity<String> sendImageSearchRequest(MultipartBodyBuilder bodyBuilder) {
         try {
@@ -65,13 +49,13 @@ public class ImageSearchService {
         }
     }
 
-    private MultipartBodyBuilder buildImageSearchRequestBody(ImageSearchDTO imageSearchDTO, String categoryName, List<String> styleByCategory) {
+    private MultipartBodyBuilder buildImageSearchRequestBody(ImageSearchDTO imageSearchDTO, String categoryName) {
         MultipartBodyBuilder bodyBuilder = new MultipartBodyBuilder();
         bodyBuilder.part("image_upload", imageSearchDTO.getImage().getResource());
-        bodyBuilder.part("category", categoryName);
+        bodyBuilder.part("category_name", categoryName);
+        bodyBuilder.part("category_id_list", imageSearchDTO.getCategoryList());
         bodyBuilder.part("mall_type_id", imageSearchDTO.getMallTypeId());
         bodyBuilder.part("offset", imageSearchDTO.getOffset());
-        bodyBuilder.part("style_id_list", styleByCategory);
         return bodyBuilder;
     }
 }
