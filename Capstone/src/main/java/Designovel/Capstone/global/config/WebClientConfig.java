3f8c972b1051fc179b6ff59bb8d5870e85141f3f
GeonHeeLeee <ghlee00125@naver.com>
2024-10-14 15:33:55 +0900

    feat: 배포용으로 수정

--- Capstone/src/main/java/Designovel/Capstone/global/config/WebClientConfig.java
+++ Capstone/src/main/java/Designovel/Capstone/global/config/WebClientConfig.java
@@ -13,7 +13,10 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class WebClientConfig {
 
-    private static final String fastAPIUrl = "http://115.85.180.192:8000/";
+    @Value("${FAST_API_URL}")
+    private String fastAPIUrl;
+
+//    private static final String fastAPIUrl = "http://115.85.180.192:8000/";
 
     @Bean
     public WebClient webClient(WebClient.Builder webClientBuilder) {
