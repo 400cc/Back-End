2c69a3d6bd8a6aabe2fd85b2b698922bdaae4a10
GeonHeeLeee <ghlee00125@naver.com>
2024-10-14 16:33:30 +0900

    feat: 환경변수 롤백

--- Capstone/src/main/java/Designovel/Capstone/global/config/WebClientConfig.java
+++ Capstone/src/main/java/Designovel/Capstone/global/config/WebClientConfig.java
@@ -13,10 +13,10 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class WebClientConfig {
 
-    @Value("${FAST_API_URL}")
-    private String fastAPIUrl;
+//    @Value("${FAST_API_URL}")
+//    private String fastAPIUrl;
 
-//    private static final String fastAPIUrl = "http://115.85.180.192:8000/";
+    private static final String fastAPIUrl = "http://115.85.180.192:8000/";
 
     @Bean
     public WebClient webClient(WebClient.Builder webClientBuilder) {
