eec59626e657199d6be0c1f628895fb9428b59ee
GeonHeeLeee <ghlee00125@naver.com>
2024-10-15 15:10:49 +0900

    feat: 로그 추가 및 로그 레벨 변경

--- Capstone/src/main/java/Designovel/Capstone/domain/mallType/enumType/MallTypeId.java
+++ Capstone/src/main/java/Designovel/Capstone/domain/mallType/enumType/MallTypeId.java
@@ -3,9 +3,11 @@ package Designovel.Capstone.domain.mallType.enumType;
 import Designovel.Capstone.global.exception.CustomException;
 import Designovel.Capstone.global.exception.ErrorCode;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@Slf4j
 public enum MallTypeId {
     MUSINSA("JN1qnDZA"),
     WCONCEPT("l8WAu4fP"),
@@ -23,6 +25,7 @@ public enum MallTypeId {
                 return;
             }
         }
+        log.error("유효하지 않은 쇼핑몰: {}", type);
         throw new CustomException(HttpStatus.BAD_REQUEST, ErrorCode.INVALID_MALL_TYPE_ID);
     }
 }
