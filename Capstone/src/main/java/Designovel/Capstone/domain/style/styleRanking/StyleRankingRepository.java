e35c180a3bc1d2e811c2ba2cbb3584e514c0c0a0
GeonHeeLeee <ghlee00125@naver.com>
2024-10-22 09:04:57 +0900

    feat: 브랜드 조회 정렬 로직 작성

--- Capstone/src/main/java/Designovel/Capstone/domain/style/styleRanking/StyleRankingRepository.java
+++ Capstone/src/main/java/Designovel/Capstone/domain/style/styleRanking/StyleRankingRepository.java
@@ -18,7 +18,7 @@ public interface StyleRankingRepository extends JpaRepository<StyleRanking, Inte
      * @param mallTypeId
      * @return
      */
-    @Query("select distinct p.brand from StyleRanking p where p.categoryStyle.style.id.mallTypeId = :mallTypeId")
+    @Query("select distinct p.brand from StyleRanking p where p.categoryStyle.style.id.mallTypeId = :mallTypeId order by p.brand asc")
     List<String> findDistinctBrand(@Param("mallTypeId") String mallTypeId);
 
     /**
